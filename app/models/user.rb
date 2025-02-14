ba8cbfbbe6344e70e348bc43bcc9e8eb03b65379
s-shiroishi <138426968+s-shiroishi@users.noreply.github.com>
2025-02-14 13:33:44 +0000

    Finish chap10

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },allow_nil: true
 
   def self.digest(string)
     cost = if ActiveModel::SecurePassword.min_cost
@@ -24,6 +24,7 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
   end
 
   def authenticated?(remember_token)
@@ -33,5 +34,9 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def session_token
+    remember_digest || remember
   end
 end
